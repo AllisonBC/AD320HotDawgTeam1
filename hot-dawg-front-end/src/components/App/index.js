16dbc70ee4b567406cbbd0d6bff3756a1344ffef
Alexander Sutherland <alexandernsutherland@gmail.com>
2020-02-17 22:47:52 -0800

    added map directory to post future google map

--- hot-dawg-front-end/src/components/App/index.js
+++ hot-dawg-front-end/src/components/App/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to our Hot Dawg Cart!</h1>
-
+        
         
       </header>
     </div>

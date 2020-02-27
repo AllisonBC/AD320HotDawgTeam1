aa23982c4f6fb4f7fbead9eea80155cb0baf3491
Alexander Sutherland <alexandernsutherland@gmail.com>
2020-02-27 08:35:41 -0800

    combined mine and luca's front end stuff

--- hot-dawg-front-end/src/components/App/index.js
+++ hot-dawg-front-end/src/components/App/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Contact from '../Contact';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to our Hot Dawg Cart!</h1>
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">HotDogApp</Link>} scroll>
+            <Navigation>
+                <Link to="/History">History</Link>
+                <Link to="/Menu">Menu</Link>
+                <Link to="/Vendors">Vendors</Link>
+                <Link to="/contact">Carts</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">HotDogApp</Link>}>
+            <Navigation>
+              <Link to="/History">History</Link>
+              <Link to="/Menu">Menu</Link>
+              <Link to="/Vendors">Vendors</Link>
+              <Link to="/carts">Carts</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Contact/>
+        </Content>
+    </Layout>
+</div>
 
-        
-      </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

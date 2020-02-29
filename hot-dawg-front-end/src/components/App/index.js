2eeda16385a8221323d567f197089f2aab212a3c
Alexander Sutherland <alexandernsutherland@gmail.com>
2020-02-29 13:39:57 -0800

    up to date

--- hot-dawg-front-end/src/components/App/index.js
+++ hot-dawg-front-end/src/components/App/index.js
@@ -11,13 +11,8 @@ class App extends Component {
     return (
       <div className="App">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} 
-          to="/">HotDogApp</Link>} scroll>
-        </Header>
-        <Map />
         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">HotDogApp</Link>}>
             <Navigation>
-              <Link to="/History">About</Link>
               <Link to="/Menu">Admin Login</Link>
               <Link to="/Vendors">Vendor Login</Link>
             </Navigation>
--- hot-dawg-front-end/src/components/Header/index.js
+++ hot-dawg-front-end/src/components/Header/index.js
@@ -4,27 +4,22 @@ import dawg from './images/dawg.png';
 import Map from '../Map';
 
 
-class Landing extends Component {
+class Header extends Component {
   render() {
     return(
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
-          
-            <div className="banner-text">
-              <h1>Sup Dawg</h1>
-
-            <hr/>
-
-          <p> Chicago-style | Dodger Dog | Corn dogs | Ham Dogs </p>
-          <Map />
-      
+          <div className="banner-text">
+              <h1>Sup Dawg</h1>\
+              <hr/>
+              <p>Chicago-style | Dodger Dog | Corn dogs | Ham Dogs </p>
             </div>
-          </Cell>
+          </Cell>         
         </Grid>
       </div>
     )
   }
 }
 
-export default Landing;
+export default Header;
